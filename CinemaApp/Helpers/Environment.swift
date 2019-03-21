c53302ae466855f566747a951693bbe1c3b000a7
Adrian Wakefield <awakefield95@gmail.com>
2019-03-21 15:00:03 +1100

    Added environment definition to distinguish between testing/production.

---
+++ CinemaApp/Helpers/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  CinemaApp
+//
+//  Created by Adrian Wakefield on 21/3/19.
+//  Copyright © 2019 Adrian Wakefield. All rights reserved.
+//
+
+struct Environment {
+    
+    static func isProduction() -> Bool {
+        return false
+    }
+}
